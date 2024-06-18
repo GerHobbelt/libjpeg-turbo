abc11c140d946acc8db8857bc9d05a0fed339b5b
Erik Olofsson <erik@olofsson.info>
2024-06-18 15:34:41 +0200

    Fix race condition initializing simd_support without using thread locals

--- simd/mips64/jsimd.c
+++ simd/mips64/jsimd.c
@@ -24,8 +24,6 @@
 
 #include <ctype.h>
 
-static THREAD_LOCAL unsigned int simd_support = ~0;
-
 #if defined(__linux__)
 
 #define SOMEWHAT_SANE_PROC_CPUINFO_SIZE_LIMIT  (1024 * 1024)
@@ -60,12 +58,12 @@ check_feature(char *buffer, char *feature)
 }
 
 LOCAL(int)
-parse_proc_cpuinfo(int bufsize)
+parse_proc_cpuinfo(int bufsize, unsigned int *out_simd_support)
 {
   char *buffer = (char *)malloc(bufsize);
   FILE *fd;
 
-  simd_support = 0;
+  *out_simd_support = 0;
 
   if (!buffer)
     return 0;
@@ -80,7 +78,7 @@ parse_proc_cpuinfo(int bufsize)
         return 0;
       }
       if (check_feature(buffer, "loongson-mmi"))
-        simd_support |= JSIMD_MMI;
+        *out_simd_support |= JSIMD_MMI;
     }
     fclose(fd);
   }
@@ -89,6 +87,8 @@ parse_proc_cpuinfo(int bufsize)
 }
 
 #endif
+
+static jsimd_atomic_uint simd_support = ~0;
 
 /*
  * Check what SIMD accelerations are supported.
@@ -106,7 +106,7 @@ init_simd(void)
   if (simd_support != ~0U)
     return;
 
-  simd_support = 0;
+  unsigned int new_simd_support = 0;
 
 #if defined(__linux__)
   while (!parse_proc_cpuinfo(bufsize)) {
@@ -117,18 +117,20 @@ init_simd(void)
 #elif defined(__mips_loongson_vector_rev)
   /* Only enable MMI by default on non-Linux platforms when the compiler flags
    * support it. */
-  simd_support |= JSIMD_MMI;
+  new_simd_support |= JSIMD_MMI;
 #endif
 
 #ifndef NO_GETENV
   /* Force different settings through environment variables */
   env = getenv("JSIMD_FORCEMMI");
   if ((env != NULL) && (strcmp(env, "1") == 0))
-    simd_support = JSIMD_MMI;
+    new_simd_support = JSIMD_MMI;
   env = getenv("JSIMD_FORCENONE");
   if ((env != NULL) && (strcmp(env, "1") == 0))
-    simd_support = 0;
+    new_simd_support = 0;
 #endif
+
+  simd_support = new_simd_support;
 }
 
 GLOBAL(int)
